fd27c6d9d7d780e1ead5c6f653324b926917d960
urekanfais <102501169+urekanfais@users.noreply.github.com>
2022-03-28 11:23:49 +0700

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charsrt="utf-8"/>
+    <meta http-equiv="X-AU-Compatible"content="IE*edge"/>
+        <title>Hotel MiHoYo</title>Hotel MiHoYo
+      <link rel="stylesheet" href="css/login.cs">
+      </head>
+      <style>
+           body{
+        margin: 0;
+        padding: 0;
+         background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+      }
+      header{
+background-color:red;
+display:table;
+width:100%;
+      }
+      header p{
+        color: whitesmoke;
+        text-align: center;
+        font-weight: bold;
+        font-size:20px;
+      }
+.JARAK{
+}
+    .container{
+      background-color:;
+      width:100%;
+      text-align: center;
+      height: 1000%;
+    }
+    .container h2{
+      font-size:25px;
+      font-weight:bold;
+      text-align:center;
+      padding-top:25px;
+      color:whitesmoke;
+  }
+   .container h3{
+      font-size:25px;
+      font-weight:bold;
+      text-align:center;
+      padding-top:2px;
+      color: whitesmoke;
+    } 
+    .container input{
+      width:100%;
+      text-align:center;
+      height:40px;
+      padding: 0;
+      margin: 0;
+    }
+    .JARAK2{
+        padding-top:50px;
+    }
+    .JARAK1 button{
+      width:100%;
+      height:50px;
+      font-size:20px;
+      font-weight:bold;
+      background:red;
+      color: whitesmoke;
+    }
+    .JARAK1 button a{
+ font-weight:bold;
+     color: whitesmoke;
+    }
+        .regis  p a {
+        float:left;
+        font-size: 17px;
+        font-weight: bold;
+        color:whitesmoke;
+        text-decoration: none;
+        padding-top: 20px;
+        padding-left:1200px;
+    }
+    .spasi{
+        padding:75px;
+    }
+    .warna{
+        background
+        height:50px;
+    }
+    .warna h2{
+        font-weight: bold;
+        font-size: 18px;
+        color:whitesmoke;
+        text-align: center;
+        padding-top:7px;
+        }
+      </style>
+      <body  background="FOTO1/1234567.JPg">
+      <header>
+        <p>Hotel MiHoYo</p>
+    </header>
+<div class="JARAK"></div>
+      <div class="container">
+<h2>Email</h2>
+<input type="text" name="Email" placeholder="Email Adress">
+<h3>Password</h3>
+<input type="Password" name="Pawssword" placeholder=Password>
+<div class="JARAK2">
+    
+</div>
+</div>
+<div class="JARAK1" >
+<h4><a href="utama.html"><button type="submit" name="Login">Login</a></button></h4>
+</div>
+<div class="regis">
+    <p><a href="daftar.html">Registrasi Accounts</p></a> 
+     <p><a href="">Lupa Kata Sandi</p></a>
+
+</div>
+<div class="spasi">
+    
+</div>
+</body>
+
+</html>
